492b1b0b78038011a48d9efee17ddf948e64b597
智兵 吕 <zhibinglv@192.168.0.11>
2017-12-23 13:01:18 +0800

    添加了livecenter数据抓取的实现

---
+++ myapp/livecenter.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+import json
+import urllib
+import urllib2
+
+douyu = "http://open.douyucdn.cn/api/RoomApi/room/"
+panda = "https://route.showapi.com/1369-1?showapi_appid=52218&showapi_sign=61a557c732a244c0ba0659d313a68f0c&page="
+def getDouyuRoomInfo(room_number):
+    returnObj = {}
+    url = douyu + str(room_number)
+    req = urllib2.Request(url)
+    req.add_header("User-Agent","Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36") 
+    res_data = urllib2.urlopen(req)
+    res = res_data.read()
+    data = json.loads(res)
+    room_status = data['data']['room_status']
+    if(int(room_status) == 1):
+        returnObj['room_thumb'] = data['data']['room_thumb']
+        returnObj['owner_name'] = data['data']['owner_name']
+        returnObj['room_name'] = data['data']['room_name']
+        returnObj['online'] = data['data']['online']
+        returnObj['cate_name'] = data['data']['cate_name']
+        return returnObj
+    else:
+        return -1
+    
+def getPandaRoomInfo(room_number):
+    returnObj = {}
+    flag = False
+    tempData = {}
+
+    for i in range(0,10):
+        if(flag):
+            break
+        url = panda + str(i)
+        req = urllib2.Request(url)
+        req.add_header("User-Agent", "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36")
+        res_data = urllib2.urlopen(req)
+        res = res_data.read()
+        data = json.loads(res)
+        infoArr = data['showapi_res_body']['data']
+        for i in infoArr:
+            if(int(i['id']) == room_number):
+                flag = True
+                tempData = i
+                break
+
+    if(tempData != {}):
+        returnObj['room_thumb'] = i['pictures']['img']
+        returnObj['owner_name'] = i['userinfo']['nickName']
+        returnObj['room_name'] = i['name']
+        returnObj['online'] = i['person_num']
+        returnObj['cate_name'] = i['classification']['cname']
+        return returnObj
+    else:
+        return -1
+                
+print getPandaRoomInfo(1270429)
